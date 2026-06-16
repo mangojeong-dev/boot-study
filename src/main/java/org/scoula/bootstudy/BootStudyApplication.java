e7cc4298c39b879e8f5256811e3ed05f0aa6c3a3
mangojeong <wkdgnl123@cau.ac.kr>
2026-06-16 17:52:26 +0900

    Health API 만들어야함

--- src/main/java/org/scoula/bootstudy/BootStudyApplication.java
+++ src/main/java/org/scoula/bootstudy/BootStudyApplication.java
@@ -2,12 +2,11 @@ package org.scoula.bootstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// @SpringBootApplication은 Boot 어플리케이션 기준점이다.
+// 해당 클래스가 있는 패키지 아래를 기준으로 컴포넌트 스캔 진행
 @SpringBootApplication
 public class BootStudyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BootStudyApplication.class, args);
     }
-
 }
---
+++ src/main/java/org/scoula/bootstudy/controller/HealthController.java
@@ -0,0 +1,10 @@
+package org.scoula.bootstudy.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+}
